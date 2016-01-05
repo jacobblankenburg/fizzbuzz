e2f82c0b5d95c5cfadd9d3fc85ff80ed04f803e1
Your Name <you@example.com>
2016-01-05 10:42:48 -0600

    First Upload

---
+++ fizzbuzz.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var count
+
+for (var count=1; count <= 100; count++)
+{
+    if (count % 15 === 0)  
+         console.log ("FizzBuzz");
+    else if (count % 5 === 0) 
+         console.log ("Buzz");
+	else if (count % 3 === 0) 
+        console.log ("Fizz");
+    else
+         console.log (count)
+} 
+
+ 
